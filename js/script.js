0d5b459e2524661d7d104fb210e667e60f0cfbed
erikanto <antoerik852@gmail.com>
2025-01-13 20:10:10 +0700

    validasi nama

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // ini file javascript
+
+function replaceNama() {
+    let name = prompt("Siapakah nama Anda?", "");
+    if (name !== null && name.trim() !== "") {
+        document.getElementById("userGreeting").innerText = name;
+    } else {
+        alert("Nama tidak boleh kosong.");
+    }
+}
